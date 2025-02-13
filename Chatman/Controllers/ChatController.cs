db48bd94934a7658805153f4c4069ce11666f85c
Ann <s32154104@gmail.com>
2025-02-13 23:42:01 +0800

    feat: index

---
+++ Chatman/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chatman.Controllers
+{
+    public class ChatController : WebController
+    {
+        public IActionResult PrivateRoom()
+        {
+            return View();
+        }
+    }
+}
